4b7d7d1700a3cedb7bec1ede0749dc87d0d6a81c
313793439@qq.com <313793439@qq.com>
2019-06-24 15:42:38 +0800

    Update PythonApplication1.py

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -16,4 +16,12 @@ def fetch_spam_data(spam_url=SPAM_URL, spam_path=SPAM_PATH):
         tar_bz2_file.extractall(path=SPAM_PATH)
         tar_bz2_file.close()
 
-fetch_spam_data()
+#fetch_spam_data()
+
+HAM_DIR = os.path.join(SPAM_PATH, "easy_ham")
+SPAM_DIR = os.path.join(SPAM_PATH, "spam")
+ham_filenames = [name for name in sorted(os.listdir(HAM_DIR)) if len(name) > 20]
+spam_filenames = [name for name in sorted(os.listdir(SPAM_DIR)) if len(name) > 20]
+print(len(ham_filenames))
+print(len(spam_filenames))
+
