110de8b57e6b1b9edeabef0bb5034e4a8f585778
313793439@qq.com <313793439@qq.com>
2019-06-25 16:57:30 +0800

    update

--- PythonApplication1/PythonApplication1/module1.py
+++ PythonApplication1/PythonApplication1/module1.py
@@ -2,7 +2,7 @@ from module2 import openemail
 import joblib
 import sys
 m = sys.argv[1]
-mo = joblib.load('test.pkl')
+mo = joblib.load('C:\\Users\\lenovo\\Documents\\GitHub\\AntiSystem\\PythonApplication1\\PythonApplication1\\test.pkl')
 a = mo.predict(openemail(m))
 if a[0] == 1:
     print("垃圾邮件")
